0689c702558f4e9ad192bb8cf9dbac7bd595ba00
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-10 15:15:49 -0300

    feat: changed main method

---
+++ lib/app/fwc_album_app.dart
@@ -0,0 +1,20 @@
+import 'package:cup_flutter_app/app/core/theme/theme_config.dart';
+import 'package:cup_flutter_app/app/pages/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class FwcAlbumApp extends StatelessWidget {
+  const FwcAlbumApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FIFA World Cup Album',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeConfig.theme,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashPage(),
+      },
+    );
+  }
+}
