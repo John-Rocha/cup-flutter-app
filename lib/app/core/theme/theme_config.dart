8ae8cf099274777f879f06676fe11669427bc52f
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-10 17:28:14 -0300

    feat: configured theme

---
+++ lib/app/core/theme/theme_config.dart
@@ -0,0 +1,44 @@
+import 'package:cup_flutter_app/app/core/styles/colors_app.dart';
+import 'package:cup_flutter_app/app/core/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class ThemeConfig {
+  ThemeConfig._();
+
+  static final _defaultInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(18),
+    borderSide: BorderSide(
+      color: ColorsApp.instance.greyDark,
+      width: 1,
+    ),
+  );
+
+  static final theme = ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      iconTheme: const IconThemeData(color: Colors.black),
+      titleTextStyle: TextStyles.instance.textPrimaryFontBold.copyWith(
+        color: Colors.black,
+        fontSize: 18,
+      ),
+    ),
+    primaryColor: ColorsApp.instance.primary,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: ColorsApp.instance.primary,
+      primary: ColorsApp.instance.primary,
+      secondary: ColorsApp.instance.secondary,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      fillColor: Colors.white,
+      filled: true,
+      isDense: true,
+      contentPadding: const EdgeInsets.all(13),
+      labelStyle: TextStyles.instance.labelTextFields,
+      border: _defaultInputBorder,
+      enabledBorder: _defaultInputBorder,
+      focusedBorder: _defaultInputBorder,
+    ),
+  );
+}
