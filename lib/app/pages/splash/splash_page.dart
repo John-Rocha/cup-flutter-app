dc3fcae7a566cd05b7c64d00e2233d48836ad605
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-10 17:27:37 -0300

    feat: created splash page widget

---
+++ lib/app/pages/splash/splash_page.dart
@@ -0,0 +1,33 @@
+import 'package:cup_flutter_app/app/core/styles/button_styles.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text('Splash Page'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              style: ButtonStyles.instance.primaryButton,
+              onPressed: () {},
+              child: const Text('Salvar'),
+            ),
+            OutlinedButton(
+              style: ButtonStyles.instance.primaryOutlineButton,
+              onPressed: () {},
+              child: const Text('Salvar'),
+            ),
+            const TextField(),
+          ],
+        ),
+      ),
+    );
+  }
+}
