eadff1066fbfb03d7117838443cc17d5b0f558dd
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-11 12:18:01 -0300

    feat: configured environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:cup_flutter_app/app/core/config/env/env.dart';
 import 'package:cup_flutter_app/app/fwc_album_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  await Env.instance.load();
   runApp(const FwcAlbumApp());
 }
