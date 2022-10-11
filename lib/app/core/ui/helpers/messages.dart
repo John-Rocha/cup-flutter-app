6611ca3134c23dcb703854ce6295961bde70ad6d
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-11 09:23:40 -0300

    feat: created helpers for UI

---
+++ lib/app/core/ui/helpers/messages.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
+import 'package:cup_flutter_app/app/core/ui/styles/colors_app.dart';
+
+mixin Messages<T extends StatefulWidget> on State<T> {
+  var isOpen = false;
+
+  void showError(String message) {
+    showTopSnackBar(
+      context,
+      CustomSnackBar.error(
+        message: message,
+        backgroundColor: context.colors.primary,
+      ),
+    );
+  }
+
+  void showInfo(String message) {
+    showTopSnackBar(
+      context,
+      CustomSnackBar.info(
+        message: message,
+      ),
+    );
+  }
+
+  void showSeccess(String message) {
+    showTopSnackBar(
+      context,
+      CustomSnackBar.success(
+        message: message,
+      ),
+    );
+  }
+}
